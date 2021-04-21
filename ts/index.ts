0ec94402e244ec77f77c13077af4e0ede661c4ba
doinkythederp <mcclelll@icloud.com>
2021-04-20 22:14:22 -0700

    Finish classes and exports

---
+++ ts/index.ts
@@ -0,0 +1,148 @@
+interface LogOptions {
+	location?: string,
+	name?: string,
+	cleanMsg?: boolean
+}
+
+export const ColorList: { [key: string]: string } = {
+	reset: "0",
+	/* Foreground */
+	black: "30",
+	red: "31",
+	green: "32",
+	yellow: "33",
+	blue: "34",
+	magenta: "35",
+	cyan: "36",
+	white: "37",
+	/* Background */
+	blackBg: "40",
+	redBg: "41",
+	greenBg: "42",
+	yellowBg: "43",
+	blueBg: "44",
+	magentaBg: "45",
+	cyanBg: "46",
+	whiteBg: "47",
+	/* FX */
+	bold: "1",
+	faint: "2",
+	dim: "2", // same as faint
+	italic: "3",
+	underline: "4",
+	blinking: "5",
+	flashing: "5", // same as blinking
+	inverse: "7",
+	reverse: "7", // same as inverse
+	invisible: "8"
+}
+
+export function Color(name: string, bright: boolean = false) {
+	return ("\u001B[" + ColorList[name] + (bright ? ";1m" : "m"));
+}
+
+export interface ConsoleLike {
+	log(message: any, ...optionalParams: any[]): void,
+	info(message: any, ...optionalParams: any[]): void,
+	warn(message: any, ...optionalParams: any[]): void,
+	error(message: any, ...optionalParams: any[]): void
+}
+
+export class Console {
+	constructor(logger?: ConsoleLike) {
+		this.console = logger || console;
+	}
+
+	/** 
+	 * The node console this was instantiated with
+	 */
+	console: ConsoleLike;
+
+	/**
+	 * Log something to the console
+	 * @example
+	 * log("Connected!")
+	 */
+	log(messages: any[], options?: LogOptions): this
+	log(message: any, options: LogOptions = {}): this {
+		if (!Array.isArray(message)) message = [message];
+
+		let prefix = `${Color("reset")}LOG   ❱ `
+			.concat(options.location ? `${options.location}` : "")
+			.concat((options.location && options.name) ? " " : "")
+			.concat(options.name ? `(${options.name})` : "")
+			.concat((options.location || options.name) ? " ❱" : "")
+			.concat(Color("reset"));
+
+		this.console.log(prefix, ...message, Color("reset"));
+
+		return this;
+
+	}
+
+	/**
+	 * Log an error to the console
+	 * @example
+	 * error(new Error("Oops!"))
+	 */
+	error(messages: any[], options?: LogOptions): this
+	error(message: any, options: LogOptions = {}): this {
+		if (!Array.isArray(message)) message = [message];
+
+		let prefix = `${Color("reset") + Color("red", true)}ERROR ❱ `
+			.concat(options.location ? `${options.location}` : "")
+			.concat((options.location && options.name) ? " " : "")
+			.concat(options.name ? `(${options.name})` : "")
+			.concat((options.location || options.name) ? " ❱" : "")
+			.concat(Color("reset"));
+
+		this.console.error(prefix, ...message, Color("reset"));
+
+		return this;
+
+	}
+
+	/**
+	 * Log non-essential information to the console
+	 * @example
+	 * info("There are 12 players connected")
+	 */
+	info(messages: any[], options?: LogOptions): this
+	info(message: any, options: LogOptions = {}): this {
+		if (!Array.isArray(message)) message = [message];
+
+		let prefix = `${Color("reset") + Color("blue")}INFO  ❱ `
+			.concat(options.location ? `${options.location}` : "")
+			.concat((options.location && options.name) ? " " : "")
+			.concat(options.name ? `(${options.name})` : "")
+			.concat((options.location || options.name) ? " ❱" : "")
+			.concat(Color("reset"));
+
+		this.console.info(prefix, ...message, Color("reset"));
+
+		return this;
+
+	}
+
+	/**
+	 * Log a non-critical error or warning to the console
+	 * @example
+	 * warn("Please update to the latest version")
+	 */
+	warn(messages: any[], options?: LogOptions): this
+	warn(message: any, options: LogOptions = {}): this {
+		if (!Array.isArray(message)) message = [message];
+
+		let prefix = `${Color("reset") + Color("yellow")}WARN  ❱ `
+			.concat(options.location ? `${options.location}` : "")
+			.concat((options.location && options.name) ? " " : "")
+			.concat(options.name ? `(${options.name})` : "")
+			.concat((options.location || options.name) ? " ❱" : "")
+			.concat(Color("reset"));
+
+		this.console.warn(prefix, ...message, Color("reset"));
+
+		return this;
+
+	}
+}
